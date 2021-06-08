81b6a49e92839b89d230d83e940597dca9d9227f
Tobias Weigl <weigl@akaryon.com>
2021-06-08 13:14:14 +0200

    node server setup

---
+++ backend/server.js
@@ -0,0 +1,143 @@
+'use strict';
+// import external modules
+const WebSocketServer = require('websocket').server;
+const http = require('http');
+const { htmlEntities, colors } = require('./util');
+
+// Port where we'll run the websocket server
+const { WEBSOCKET_SERVER_PORT } = require('./util/config');
+// Origin
+const WEBSOCKET_ORIGIN = 'dubble';
+
+// Optional. You will see this name in e.g. 'ps' or 'top' command
+process.title = 'node-websocket';
+
+/**
+ * Global variables
+ */
+// latest 100 messages
+let history = [];
+// list of currently connected clients (users)
+let clients = [];
+
+/**
+ * HTTP Server as base for our WebSocket
+ */
+const httpServer = http.createServer((request, response) => {
+  // we do not need to handle any requests specifically here, as we only need the http server to initialize the websocket connection!
+  response.writeHead(501); // 501 = NOT IMPLEMENTED
+  response.end();
+});
+httpServer.listen(WEBSOCKET_SERVER_PORT, () => {
+  console.log(
+    `Server is up an running on http://localhost:${WEBSOCKET_SERVER_PORT}`
+  );
+});
+
+/**
+ * WebSocket Server
+ */
+const wsServer = new WebSocketServer({
+  // WebSocket server is tied to a HTTP server. WebSocket request is just an enhanced HTTP request.
+  // For more info http://tools.ietf.org/html/rfc6455#page-6
+  httpServer,
+});
+
+// This callback function is called every time someone tries to connect to the WebSocket server
+wsServer.on('request', (request) => {
+  // use try/catch to not kill server on any error!
+  try {
+    console.log(`${new Date()}: connection from origin [${request.origin}].`);
+
+    // accept connection - you should check 'request.origin' to make sure that client is connecting from your website
+    // (http://en.wikipedia.org/wiki/Same_origin_policy)
+    const connection = request.accept(WEBSOCKET_ORIGIN, request.origin);
+
+    // we need to know the client index to remove them on 'close' event
+    const index = clients.push(connection) - 1;
+    let userName;
+    let userColor;
+
+    console.log(`${new Date()}: Connection accepted.`);
+
+    // send chat history to new use
+    if (history.length > 0) {
+      connection.sendUTF(JSON.stringify({ type: 'history', data: history }));
+    }
+
+    connection.on('message', (messageRAW) => {
+      // take care we will receive raw (plain text) message and we will need to extract json string from message body (utf8Data)
+      console.log(messageRAW);
+
+      let message = '';
+      try {
+        message = JSON.parse(messageRAW.utf8Data);
+      } catch (error) {
+        console.error('Invalid data from client:', messageRAW.utf8Data);
+        return;
+      }
+
+      if (message.type === 'incoming-message') {
+        // accept only 'text' in this example
+        // first message sent from user is their name
+        if (!userName) {
+          // 'save' userName of this connection
+          userName = htmlEntities(message.data);
+          // get random color and send it back to the user
+          userColor = colors.shift();
+          connection.sendUTF(
+            JSON.stringify({ type: 'color', data: userColor })
+          );
+          console.log(
+            `${new Date()}: User nr ${index} has name '${userName}' and color '${userColor}'`
+          );
+        } else {
+          console.log(
+            `${new Date()}: received new message from user '${userName}': ${
+              message.data
+            }`
+          );
+          // we want to keep all sent messages in history
+          const obj = {
+            time: new Date().getTime(),
+            text: htmlEntities(message.data),
+            author: userName,
+            color: userColor,
+          };
+          history.push(obj);
+          history = history.slice(-100); // keep only the latest 100 messages
+
+          // broadcast new message to all connected clients
+          const json = JSON.stringify({ type: 'chat-message', data: obj });
+          clients.forEach((client) => {
+            client.sendUTF(json);
+          });
+        }
+      } else {
+        // send back an error information for client
+        const error = JSON.stringify({
+          type: 'error',
+          data: 'only messages of type "incoming messages" are allowed!',
+        });
+        connection.sendUTF(error);
+      }
+    });
+
+    // user disconnected at Event 'close'
+    connection.on('close', (close) => {
+      // check if user was ever correct connected
+      if (userName && userColor) {
+        console.log(
+          `${new Date()}: Peer ${connection.remoteAddress} disconnected.`
+        );
+
+        // remove user from list of connected clients
+        clients.splice(index, 1);
+        // push back user's color to be reused by another user
+        colors.push(userColor);
+      }
+    });
+  } catch (error) {
+    console.error(`${new Date()}: error occured:`, error);
+  }
+});
---
+++ frontend/client.js
@@ -0,0 +1,147 @@
+'use strict';
+
+import { SERVER_PORT } from './server-port.js';
+
+const $content = $('#content');
+const $input = $('#input');
+const $status = $('#status');
+const $count = $('#count');
+
+$status.text('Connecting...');
+
+// my color, assigned by the server
+let myColor;
+// my name, sent to the server after websocket is connected
+let myName;
+
+// if user is running mozilla then use it's built-in WebSocket
+window.WebSocket = window.WebSocket || window.MozWebSocket;
+
+// if browser doesn't support WebSocket, just show some notification and exit
+if (!window.WebSocket) {
+  $content.html(
+    $('<p>', { text: "Sorry, but your browser doens't support WebSocket!" })
+  );
+  $input.hide();
+  $status.hide();
+  throw new Error('no WebSocket support available');
+}
+
+// open connection
+const connection = new WebSocket(`ws://127.0.0.1:${SERVER_PORT}`, 'msd-webservice');
+
+// WebSocket Event 'open' and EventHandler 'onOpen'
+connection.onopen = () => {
+  $input.removeAttr('disabled');
+  $input.val('');
+  $input.attr('placeholder', 'choose name');
+  $status.text('Choose name: ');
+};
+
+// WebSocket Event 'error' and EventHandler 'onError'
+connection.onerror = (error) => {
+  // when some error occures, respond to it
+  console.error(error);
+  $status.text('ERROR');
+  $content.html(
+    $('<p>', {
+      text:
+        'Sorry, but a problem occured with your connection, or the server is down',
+    })
+  );
+};
+
+// WebSocket Event 'message' and EventHandler 'onMessage'
+// most important part - incoming messages from server
+connection.onmessage = (message) => {
+  /**
+   * try to parse JSON message. Because we know that the server
+   * always returns JSON this should work without any problem but
+   * we should make sure that the massage is not chunked or
+   * otherwise damaged.
+   */
+  let json;
+  try {
+    json = JSON.parse(message.data);
+    console.log(json);
+  } catch (error) {
+    console.error('Invalid data from server:', message);
+    return;
+  }
+
+  /**
+   * NOTE: if you're not sure about the object structure
+   * check the server source code!
+   */
+
+  switch (json.type) {
+    // first response from server should contain user's color
+    case 'color':
+      myColor = json.data;
+      $status.text(`${myName}: `).css('color', myColor);
+      $input.attr("placeholder", 'input your message');
+      $input.removeAttr('disabled').focus();
+      break;
+    // entire message history after connection
+    case 'history':
+      json.data.forEach((chatMsg) => {
+        addMessage(
+          chatMsg.author,
+          chatMsg.text,
+          chatMsg.color,
+          new Date(chatMsg.time)
+        );
+      });
+      break;
+    // single message from server
+    case 'chat-message':
+      $input.removeAttr('disabled').focus();
+      const chatMsg = json.data;
+      addMessage(
+        chatMsg.author,
+        chatMsg.text,
+        chatMsg.color,
+        new Date(chatMsg.time)
+      );
+      break;
+    default:
+      console.warn("Hmm, doesn't look like valid data", json);
+  }
+};
+
+/**
+ * Send message when user press Enter key
+ */
+$input.keydown(function (event) {
+  if (event.keyCode === 13) {
+    const msg = $(this).val();
+    if (!msg) {
+      return;
+    }
+
+    // send message as ordinary text -> use json string, to enrich message information
+    connection.send(JSON.stringify({ type: 'incoming-message', data: msg }));
+    $(this).val('');
+    $(this).attr('disabled', 'disabled');
+
+    // we know that the first message has to be the users name
+    if (!myName) {
+      myName = msg;
+    }
+  }
+});
+
+function addMessage(author, msg, color, timestamp) {
+  const self = author === myName ? ' self' : '';
+  $content.prepend(`<p class="chat-message ${self}"><span class="chat-name" style="color: ${color};">
+  ${author}</span> @ [${formattedTimeStamp(timestamp)}] : ${msg}</p>`);
+}
+
+function formattedTimeStamp(date) {
+  function lpad(num, size = 2, pad = '0') {
+    return `${num}`.length >= size
+      ? `${num}`
+      : new Array(size - `${num}`.length + 1).join(pad) + `${num}`;
+  }
+  return `${lpad(date.getHours())}:${lpad(date.getMinutes())}`;
+}
