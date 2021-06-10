68b2441706aad7577bfb2562d46784ebb0692cb5
Tobias Weigl <weigl@akaryon.com>
2021-06-10 21:30:28 +0200

    created basic database schema

--- scripts/create-database.js
+++ scripts/create-database.js
@@ -12,7 +12,20 @@ var con = mysql.createConnection({
 });
 
 const sql_string = `
-CREATE DATABASE IF NOT EXISTS dubble;
+CREATE TABLE IF NOT EXISTS users (
+  id int NOT NULL AUTO_INCREMENT,
+  username varchar(255) NOT NULL,
+  password varchar(255) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS text_item (
+  id int NOT NULL AUTO_INCREMENT,
+  text longtext NULL,
+  user_id int NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (user_id) REFERENCES users(id)
+)
 `;
 
 con.on('error', function() {
@@ -21,9 +34,28 @@ con.on('error', function() {
 con.connect(function(err) {
   if (err) throw err;
   console.log("Connected!");
-  con.query(sql_string, function (err, result) {
+  con.query("CREATE DATABASE IF NOT EXISTS dubble", function (err, result) {
     if (err) throw err;
     console.log("Database created");
     con.destroy();
   });
 });
+con.destroy();
+con = mysql.createConnection({
+  host: DATABASE_HOST,
+  user: DATABASE_USER,
+  password: DATABASE_PASSWORD,
+  database: 'dubble',
+  multipleStatements: true
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+  con.query(sql_string, function (err, result) {
+    if (err) throw err;
+    console.log("Tables created");
+    con.destroy();
+  });
+});
+con.destroy();
